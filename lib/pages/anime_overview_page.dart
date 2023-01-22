c940fdd2a1189f025d788020fd7c2528d297268a
Zackautocracy <codeautocracy@gmail.com>
2023-01-22 12:08:17 +0100

    refactor seasonal anime card into a separate widget

--- lib/pages/anime_overview_page.dart
+++ lib/pages/anime_overview_page.dart
@@ -1,7 +1,7 @@
 import 'package:animista/api/graphql_service.dart';
 import 'package:animista/api/queries/seasonal_anime.query.dart';
 import 'package:animista/models/seasonal_anime.model.dart';
-import 'package:animista/pages/anime_details_page.dart';
+import 'package:animista/widgets/seasonal_anime_card.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql/client.dart';
 
@@ -21,6 +21,8 @@ class _AnimeOverviewPage extends State<AnimeOverviewPage> {
   late List<SeasonalAnimeModel> animes;
   GraphQLService gqlService = GraphQLService();
   final String query = getSeasonalAnime;
+
+  //Todo : Get season and seasonYear dynamically
   final variables = {
     'page': 1,
     'perPage': 20,
@@ -30,17 +32,6 @@ class _AnimeOverviewPage extends State<AnimeOverviewPage> {
     'sort': ['POPULARITY_DESC'],
     'format_in': ['TV', 'OVA', 'ONA']
   };
-
-  @override
-  void initState() {
-    super.initState();
-    // final data = json.decode(seasonalAnimesJsonExample) as List<dynamic>;
-    // animes = data
-    //     .cast<Map<String, dynamic>>()
-    //     .where((element) => element['episodes'] != null)
-    //     .map((e) => SeasonalAnimeModel.fromJson(e))
-    //     .toList();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,131 +64,12 @@ class _AnimeOverviewPage extends State<AnimeOverviewPage> {
                 return ListView.builder(
                     itemCount: animes.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return seasonalAnimeCard(context, animes[index]);
+                      return SeasonalAnimeCard(data: animes[index]);
                     });
               } else if (snapshot.hasError) {
                 return Center(child: Text("${snapshot.error}"));
               }
               return const Center(child: CircularProgressIndicator());
             }));
-  }
-
-  List<Widget> _generateSeasonalCards(context) {
-    List<Widget> cards = [];
-    for (SeasonalAnimeModel anime in animes) {
-      cards.add(seasonalAnimeCard(context, anime));
-    }
-    return cards;
-  }
-
-  Widget seasonalAnimeCard(context, SeasonalAnimeModel data) {
-    return InkWell(
-        onTap: () {
-          AnimeDetailsPage.navigateTo(context);
-        },
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: const BoxDecoration(
-            border: Border(
-              bottom: BorderSide(color: Colors.grey),
-            ),
-          ),
-          child: Table(columnWidths: const {
-            0: FlexColumnWidth(35),
-            1: FlexColumnWidth(65)
-          }, children: [
-            TableRow(children: [
-              Image.network(
-                data.coverImage,
-                fit: BoxFit.cover,
-              ),
-              Padding(
-                  padding: const EdgeInsets.only(left: 15),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Flexible(
-                                  child: Text(
-                                      data.title.second ??
-                                          data.title.third ??
-                                          data.title.first ??
-                                          "",
-                                      textAlign: TextAlign.left,
-                                      style: const TextStyle(fontSize: 20))),
-                              IconButton(
-                                  //Todo: Book mark action
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.bookmark_add,
-                                    color: Colors.grey,
-                                  ))
-                            ]),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 5, bottom: 5),
-                            child: Text(
-                                "${data.format} • ${data.season} ${data.seasonYear}",
-                                textAlign: TextAlign.left,
-                                style: const TextStyle(
-                                    fontSize: 15, color: Colors.blueGrey))),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 5),
-                            child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  const Tooltip(
-                                    message: "Favourites",
-                                    child: Icon(
-                                      Icons.favorite,
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 4),
-                                    child: Text(data.favourites.toString()),
-                                  ),
-                                ])),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 5),
-                            child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  const Tooltip(
-                                    message: "Weighted Average Score",
-                                    child: Icon(
-                                      Icons.star_outlined,
-                                      color: Colors.orangeAccent,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 4, right: 32),
-                                    child: Text(data.averageScore.toString()),
-                                  )
-                                ])),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 5),
-                            child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  const Tooltip(
-                                    message: "Weighted Average Score",
-                                    child: Icon(
-                                      Icons.tv,
-                                      color: Colors.blue,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 4, right: 32),
-                                    child: Text(data.episodes.toString()),
-                                  )
-                                ])),
-                      ]))
-            ])
-          ]),
-        ));
   }
 }
---
+++ lib/widgets/seasonal_anime_card.dart
@@ -0,0 +1,121 @@
+import 'package:animista/models/seasonal_anime.model.dart';
+import 'package:animista/pages/anime_details_page.dart';
+import 'package:flutter/material.dart';
+
+class SeasonalAnimeCard extends StatelessWidget {
+  final SeasonalAnimeModel data;
+
+  const SeasonalAnimeCard({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          AnimeDetailsPage.navigateTo(context);
+        },
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: Colors.grey),
+            ),
+          ),
+          child: Table(columnWidths: const {
+            0: FlexColumnWidth(35),
+            1: FlexColumnWidth(65)
+          }, children: [
+            TableRow(children: [
+              Image.network(
+                data.coverImage,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                  child: Text(
+                                      data.title.second ??
+                                          data.title.third ??
+                                          data.title.first ??
+                                          "",
+                                      textAlign: TextAlign.left,
+                                      style: const TextStyle(fontSize: 20))),
+                              IconButton(
+                                  //Todo: Book mark action
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.bookmark_add,
+                                    color: Colors.grey,
+                                  ))
+                            ]),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 5, bottom: 5),
+                            child: Text(
+                                "${data.format} • ${data.season} ${data.seasonYear}",
+                                textAlign: TextAlign.left,
+                                style: const TextStyle(
+                                    fontSize: 15, color: Colors.blueGrey))),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Tooltip(
+                                    message: "Favourites",
+                                    child: Icon(
+                                      Icons.favorite,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 4),
+                                    child: Text(data.favourites.toString()),
+                                  ),
+                                ])),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Tooltip(
+                                    message: "Weighted Average Score",
+                                    child: Icon(
+                                      Icons.star_outlined,
+                                      color: Colors.orangeAccent,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 4, right: 32),
+                                    child: Text(data.averageScore.toString()),
+                                  )
+                                ])),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Tooltip(
+                                    message: "Weighted Average Score",
+                                    child: Icon(
+                                      Icons.tv,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 4, right: 32),
+                                    child: Text(data.episodes.toString()),
+                                  )
+                                ])),
+                      ]))
+            ])
+          ]),
+        ));
+  }
+}
